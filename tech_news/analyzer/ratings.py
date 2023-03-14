39e68ddda27e8d4684c0690221a7275586cf1a4c
jcaluiz <lacjunior129.la@gmail.com>
2023-03-14 15:04:17 -0300

    Criado uma função que liste as categorias mais existentes no banco de dados

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,17 @@
+from tech_news.database import get_collection
+
+
 # Requisito 10
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    pipeline = [
+        {"$group": {"_id": "$category", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1, "_id": 1}},
+        {"$limit": 5}
+    ]
+    collection = get_collection()
+    news = [new["_id"] for new in collection.aggregate(pipeline)]
+    return news
+
+
+if __name__ == "__main__":
+    print(top_5_categories())
