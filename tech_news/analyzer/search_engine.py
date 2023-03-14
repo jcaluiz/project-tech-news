2f494092b5bb7b1332d859c1fa466434cb7e8867
jcaluiz <lacjunior129.la@gmail.com>
2023-03-14 14:19:23 -0300

    Criado a função de pesquisar por  e retorna uma lista de tuplas com ]2;]1; e

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import search_news
+from datetime import datetime
 
 
 # Requisito 7
@@ -15,7 +16,19 @@ def search_by_title(title):
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    tuple_search = []
+    try:
+        date_obj = datetime.strptime(date, "%Y-%m-%d")
+        date_iso = date_obj.strftime("%d/%m/%Y")
+        query = {"timestamp": {"$regex": date_iso, "$options": "i"}}
+        search_list = search_news(query)
+        for search in search_list:
+            tuple_search.append(
+                (search["title"], search["url"])
+            )
+    except ValueError:
+        raise ValueError("Data inválida")
+    return tuple_search
 
 
 # Requisito 9
@@ -31,4 +44,4 @@ def search_by_category(category):
 
 
 if __name__ == "__main__":
-    print(search_by_title("notícia"))
+    print(search_by_date("2022/04/07"))
